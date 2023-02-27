6f38844a9de3943457a7f0c8b803d8c0e4bf9bd5
Ace <Temelbahadir1992@gmail.com>
2023-02-27 23:38:20 +0100

    Tast 03 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  var rString = "";
+  if (times < 0) {
+    return "ERROR";
+  } else {
+    for (let string = 0; string < times; string++) {
+      var rString = rString + word;
+    }
+    return rString;
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (word) {
+    wordArray = word.split("");
+    let reverseWord = [];
+    reverseWord = wordArray.reverse();
 
+    return reverseWord.join("");
 };
 
 // Do not edit below this line
