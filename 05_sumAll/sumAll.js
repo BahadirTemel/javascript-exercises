57c42efe5876e25d5d32ca47b36e98cc28ae7ae1
Ace <Temelbahadir1992@gmail.com>
2023-02-28 17:04:55 +0100

    Sum Numbers Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  var sum = 0;
+  if(startNum > endNum) {
+    x = startNum;
+    startNum = endNum;
+    endNum = x;
+  }
+  if (startNum < 0) {
+    return 'ERROR';
+  }
+  if (endNum < 0) {
+    return 'ERROR';
+  }
+  if (typeof endNum !== 'number' || typeof startNum!== 'number') {
+    return 'ERROR';
+  }
+  for (; startNum <= endNum; startNum++) {
+    sum += startNum;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
