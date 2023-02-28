eec09bb6cd3323cbca2c4772b5655d601b9c73b8
Ace <Temelbahadir1992@gmail.com>
2023-02-28 16:29:12 +0100

    remove from Array done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...remove) {
+  const alteredArr = arr.filter((item) => !remove.includes(item));
+  return alteredArr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
