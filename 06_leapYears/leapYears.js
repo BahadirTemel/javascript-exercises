af4ad7ba8fbdacb8a05f0ff188ce2d83eec63ff0
Ace <Temelbahadir1992@gmail.com>
2023-02-28 17:25:26 +0100

    isLeapYear done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+// Leap years are divisible by 4, but not by 100 unless it is divisible by 400.
 
 // Do not edit below this line
 module.exports = leapYears;
