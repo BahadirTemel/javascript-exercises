4f0f6ee64421b5c7321bd9ee32e6db7108aa516e
Ace <Temelbahadir1992@gmail.com>
2023-03-01 16:36:23 +0100

    Calculator Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function([...nums]) {
+  sumAll = 0;
+	if (nums === undefined || nums.length === 0) {
+    return 0;
+  }else{
+    for (let i = 0; i < nums.length; i++) {
+    sumAll += nums[i];
+  }
+};
+return sumAll;
+}
+
+const multiply = function([...nums]) {
+  multAll = 1;
+  if (nums === undefined || nums.length === 0) {
+    return 1;
+  }else{
+    for (let i = 0; i < nums.length; i++) {
+    multAll *= nums[i];
+  }
+};
+return multAll;
+}
+
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }else{
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
