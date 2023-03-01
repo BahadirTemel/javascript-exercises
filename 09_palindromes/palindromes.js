6941964a09a59436571aef73d1cb1a562af9c349
Ace <Temelbahadir1992@gmail.com>
2023-03-01 17:04:41 +0100

    Palindrome Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
-};
+const palindromes = function (words) {
+  const alteredWords = words.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return alteredWords === alteredWords.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
