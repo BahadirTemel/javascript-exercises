28ea1eaa86fa640fc4251a6ec4113b2f7a951630
Ace <Temelbahadir1992@gmail.com>
2023-03-06 22:23:35 +0100

    done

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,22 @@
-const caesar = function() {
+const caesar = function (plainText, key) {
+  var cipherText = "";
 
+  for (var i = 0; i < plainText.length; i++) {
+    var ch = plainText.charCodeAt(i);
+
+    if (ch >= 65 && ch <= 90) {
+      // uppercase
+      cipherText += String.fromCharCode(((ch - 65 + key + 26) % 26) + 65);
+    } else if (ch >= 97 && ch <= 122) {
+      // lowercase
+      cipherText += String.fromCharCode(((ch - 97 + key + 26) % 26) + 97);
+    } else {
+      // non-alphabetic characters
+      cipherText += plainText.charAt(i);
+    }
+  }
+
+  return cipherText;
 };
 
 // Do not edit below this line
