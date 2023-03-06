c8b59100bfb7e2d5072a6b611b7336e83cb65854
Ace <Temelbahadir1992@gmail.com>
2023-03-06 21:53:38 +0100

    ?

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,5 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => a.age - b.age)[0];
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
